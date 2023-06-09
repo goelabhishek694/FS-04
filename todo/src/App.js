0e062de75e4e875afbd09053e6ac0e4f138b9cd2
goelabhishek694 <goelabhishek694@gmail.com>
2023-06-09 22:08:17 +0530

    todo app uing clas components

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Components/Todo';
-
+import './Components/Todo.css';
 function App() {
   return (
     <div>
--- todo/src/Components/Todo.js
+++ todo/src/Components/Todo.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react'
-
+// uuid
 export default class Todo extends Component {
-    constructor() {
-        super() //-> constructor method of Component is called
-        this.state = {
-            isHungry:true
-        }
+  constructor() {
+    super(); //-> constructor method of Component is called
+      this.state = {
+          tasks: [{id:1,value:'Revise JavaScript'}, {id:2,value:'Revise DSA'}],
+          currTask:''
+    };
+  }
+
+  //methods in class
+    handleChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+          currTask:e.target.value
+      })
+  }
+
+    addTask = () => {
+        this.setState({
+            tasks: [...this.state.tasks, { id: this.state.tasks.length + 1, value: this.state.currTask }],
+            currTask:''
+        })
+    }
+    
+    handleDelete = (id) => {
+        const newTasks = this.state.tasks.filter(taskObj => taskObj.id != id)
+        this.setState({
+            tasks:[...newTasks]
+        })
     }
 
-    //methods in class
-
-    //render method id called initially and vevrytime when state is changed. 
+  //render method id called initially and everytime when state is changed.
   render() {
     return (
-      <div>Todo</div>
-    )
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Your Task"
+                onChange={this.handleChange}
+                value={this.state.currTask}
+        />
+            <button onClick={this.addTask}>Add Task</button>
+            {/* to render my tasks */}
+            {this.state.tasks.map((taskObj,idx) => {
+                return (
+                  <li className="list" key={taskObj.id}>
+                    <p>{`${idx + 1}. ${taskObj.value}`}</p>
+                        <button onClick={() => this.handleDelete(taskObj.id)
+                        }>
+                      Delete
+                    </button>
+                  </li>
+                );
+            })}
+      </div>
+    );
   }
 }
+
+
+// let btn = document.getElementById(add)
+// btn.addEventListener('click', addTask);
