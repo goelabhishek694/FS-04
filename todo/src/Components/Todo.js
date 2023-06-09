c1220cae5d74b36697b9c531e16ba603c5f7bfc5
goelabhishek694 <goelabhishek694@gmail.com>
2023-06-09 22:19:03 +0530

    lifecycle methods exmple

--- todo/src/Components/Todo.js
+++ todo/src/Components/Todo.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 // uuid
 export default class Todo extends Component {
-  constructor() {
+    constructor() {
+      console.log('constructor is called');
     super(); //-> constructor method of Component is called
       this.state = {
           tasks: [{id:1,value:'Revise JavaScript'}, {id:2,value:'Revise DSA'}],
-          currTask:''
+          currTask:'',
+          counter:0
     };
   }
+
+    componentDidMount() {
+        console.log('CDM is called');
+        let data = setTimeout(() => {
+            this.setState({ counter: this.state.counter + 1 })
+        },2000)
+    }
+
+    componentWillUpdate() {
+        console.log('comp will update is called just before state is chnaged');
+    }
+
+    componentDidUpdate() {
+        console.log('comp did update is called after state is changed ');
+    }
 
   //methods in class
     handleChange = (e) => {
@@ -32,7 +49,8 @@ export default class Todo extends Component {
     }
 
   //render method id called initially and everytime when state is changed.
-  render() {
+    render() {
+      console.log('render is called');
     return (
       <div>
         <input
